629f973a955a6ecaff7e1aa4446f2c1dc8d26ca5
Eduardo <eduarduholiveira@gmail.com>
2022-08-03 21:12:11 -0300

    subindo meus exercicios de condicionais

---
+++ script.js
@@ -0,0 +1,36 @@
+/*const bool1 = true
+const bool2 = false
+
+if (bool1) {alert("é verdadeira !")}
+else {alert("o bool1 é falso !!")}
+
+const bool3 = true
+if (bool1 === bool2) {
+    alert ("são iguais eeeeee!")}
+
+else if (bool2 === bool3){
+    alert("eles são iguais !!!!!!")}
+
+else if (bool1 === bool3) {
+    alert("o queé igual ao que ?")}
+
+else {
+    alert("a pronto ninguem é igual a ninguem .")}
+
+    const idade = prompt("Qual a sua Idade ?")
+
+    if (idade >= 16){
+        alert("vc pode tirar seu titulo !")
+        if (idade >= 60){
+            alert(" Mas voce não é obrigado a votar !!")}
+
+        else if (idade >= 16 && idade <= 17){
+            alert("Mas não é obrigado a votar!!")} }
+      else {alert("vc não pode tirar titulo")}*/
+
+const media = prompt("Qual a sua nota ?")
+      if (media >= 5 && media <= 10){alert("PARABÉNS VOCÊ FOI APROVADA(O)!!")}
+      else if (media >= 3 && media < 5){alert("Você esta de RECUPERAÇÃO !")}
+      else if (media < 3){alert("Que pena, você está reprovada(o).")}
+      else {alert("Dado inválido")}
+    
